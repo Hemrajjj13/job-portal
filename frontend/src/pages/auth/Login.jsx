20fa9ae911d0bcc0e3c35284f000dc3669665fec
Hemrajjj13 <iamhemraj13@gmail.com>
2026-01-30 20:03:02 +0530

    Created a mock logic for login page

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import { useAuth } from "../../context/AuthContext";
+import { useNavigate } from "react-router-dom";
+
 
 const Login = () => {
+  const { login } = useAuth();
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    // Simulated backend response
+    login({
+      id: "123",
+      name: "Hemraj",
+      role: "candidate",
+    });
+
+    navigate("dashboard/candidate");
+  };
+
   return (
-    <h1 className='p-6'>Login Page</h1>
+    <div className='p-6'>
+      <h1 className="mb-4">Login Page</h1>
+
+      <button
+        onClick={handleLogin}
+        className="px-4 py-2 bg-black text-white"
+      >
+        Login (mock)
+      </button>
+    </div>
   );
 };
 
